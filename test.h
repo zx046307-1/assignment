4a8a72ab5590a289f9d36356f63ab63a0dcc0b7a
zx046307-1 <83290042+zx046307-1@users.noreply.github.com>
2021-06-07 16:16:46 +0800

    標頭檔

---
+++ test.h
@@ -0,0 +1,37 @@
+#ifndef test_h
+#define test_h
+
+#include<string>
+
+using namespace std;
+
+class test
+{
+private:
+	int cards[53];
+	int cardsValue[53];
+	string cardsName[53];
+
+	int money;
+	int bet;
+
+	int player[5];
+	int computer[5];
+	int playerNum;
+	int computerNum;
+public:
+	test();
+	void initCards();
+	string getPlayer();
+	string getComputer();
+	int getSumF();
+	int getSumL();
+	void farmerAsk();
+	void landlordAsk();
+	void inputBet();
+	void newGame();
+	void landlordProcess();
+
+};
+
+#endif
