9d03235d8e7782710178c87e37805843217cbc49
zx046307-1 <83290042+zx046307-1@users.noreply.github.com>
2021-06-18 12:39:32 +0800

    Create multiplayer.h
    製作中...(先存個檔)

---
+++ multiplayer.h
@@ -0,0 +1,18 @@
+#ifndef multiplayer_h
+#define multiplayer_h
+
+using namespace std;
+
+#include <string>
+
+#include "test.h"
+
+class Multiplayer:public test
+{
+private:
+	int numOfPlayer;
+public:
+
+};
+
+#endif
